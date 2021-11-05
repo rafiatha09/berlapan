80dfbf0691a13f9b1e5017634dcb194e32156e16
defranssco <defran.fransisco93@gmail.com>
2021-11-05 21:49:51 +0700

    Implementation some module

---
+++ donordarah/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from donordarah import views
+
+app_name = 'donorpage'
+
+urlpatterns = [
+    path('', views.donorPage, name='donorpage'),
+    path('layananpage/', views.layananPage, name='layananpage'),
+    path('form/', views.formPage, name='form'),
+    path('outputform/', views.hasilFormPage, name='outputform'),
+]
---
+++ donordarah/views.py
@@ -0,0 +1,39 @@
+from typing import List
+from django.db import models
+from django.db.models.lookups import PostgresOperatorLookup
+from django.http import response
+from django.shortcuts import redirect, render
+
+from .forms import DonorDarahForm
+from .models import DonorForm, NamaLayanan
+
+
+def donorPage(request):
+    namaLayanan = NamaLayanan.objects.all()
+    response = {'namaLayanan' : namaLayanan}
+    return render(request, 'donorpage.html', response)
+
+def layananPage(request) :
+    namaLayanan = NamaLayanan.objects.all()
+    response = {'namaLayanan' : namaLayanan}
+    return render(request, 'layananpage.html', response)
+
+def formPage(request) :
+    context ={}
+    form = DonorDarahForm(request.POST or None, request.FILES or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect('outputform/')
+    context['form']= form
+    return render(request, "form.html", context)
+
+def hasilFormPage(request) :
+    allRelawan = DonorForm.objects.all()
+    result = DonorForm
+    for relawan in allRelawan :
+        if relawan.FullName == 'Salsabila Zahra Chinanti' :
+            result = relawan
+
+    response = {'result' : result}
+    return render(request, 'outputform.html', response)
